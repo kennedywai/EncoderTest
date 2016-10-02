b37c5f03ded1fed109704b9c687a12b039f0ea1e
Wai Kennedy <kennedywai@hotmail.com>
2016-10-02 20:07:07 +0800

    Update encoder_test.ino

--- encoder_test/encoder_test.ino
+++ encoder_test/encoder_test.ino
@@ -1,24 +1,23 @@
 /***MOTOR TEST***/
-//Right Motor
-int enA = 3;//analog
+//Left Motor
+int enA = 11;//analog
 int in1 = 7;//digital
 int in2 = 8;//digital
-int encoderR_A = 2;//interrupt pin for right motor a
-int encoderR_B = 4;//not using an interrupt pin for right motor B
-//Left Motor
-int enB = 4;//analog
+int encoderL_A = 2;//interrupt pin for right motor a
+int encoderL_B = 4;//not using an interrupt pin for left motor B
+//Right Motor
+int enB = 13;//analog
 int in3 = 9;//digital
 int in4 = 10;//digital
-int encoderL_A = 3;//interrupt pin for left motor a
-int encoderL_B = 5;//not using an interrupt pin for left motor B
+int encoderR_A = 3;//interrupt pin for left motor a
+int encoderR_B = 5;//not using an interrupt pin for right motor B
 
 // variables to store the number of encoder pulses
 // for each motor
-volatile unsigned long rightCount = 0;
 volatile unsigned long leftCount = 0;
+volatile unsigned long rightCount = 0;
 
-void setup()
-{
+void setup(){
   // set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
@@ -28,14 +27,14 @@ void setup()
   pinMode(in4, OUTPUT);
   
   // encoders
-  pinMode(encoderR_A, INPUT);
-  pinMode(encoderR_B, INPUT);
   pinMode(encoderL_A, INPUT);
   pinMode(encoderL_B, INPUT);
+  pinMode(encoderR_A, INPUT);
+  pinMode(encoderR_B, INPUT);
   
   // interrupts
-  attachInterrupt(1, readEncoderR, CHANGE);
-  attachInterrupt(0, readEncoderL, CHANGE);
+  attachInterrupt(1, readEncoderL, CHANGE);
+  attachInterrupt(0, readEncoderR, CHANGE);
   
   // 
   Serial.begin(9600);
@@ -74,8 +73,7 @@ if (digitalRead(encoderL_A) == HIGH) {
   }
 }
 
-void demoOne()
-{
+void demoOne(){
   // this function will run the motors in both directions at a fixed speed
   // turn on motor A
   digitalWrite(in1, HIGH);
@@ -101,8 +99,7 @@ void demoOne()
   digitalWrite(in4, LOW);
 }
 
-void demoTwo()
-{
+void demoTwo(){
   // this function will run the motors across the range of possible speeds
   // note that maximum speed is determined by the motor itself and the operating voltage
   // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
@@ -113,6 +110,8 @@ void demoTwo()
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH); 
   // accelerate from zero to maximum speed
+  // Arduino Leonardo-->PWM: 3, 5, 6, 9, 10, 11, and 13. 
+  // Provide 8-bit PWM output with the analogWrite() function.
   for (int i = 0; i < 256; i++)
   {
     analogWrite(enA, i);
@@ -132,19 +131,32 @@ void demoTwo()
   digitalWrite(in2, LOW);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);  
+} 
+
+void forward(){
+  //left wheel
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  //right wheel
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
-void loop()
-{
-  //demoOne();
-  //Serial.print("Demo 1\n");
-  //delay(1000);
-  //demoTwo();
-  //Serial.print("Demo 2\n");
-  //delay(1000);
-  
-  Serial.print("Right Count:");
-  Serial.println(rightCount);
+void loop(){
+  /*
+  demoOne();
+  Serial.print("Demo 1\n");
+  delay(1000);
+  Serial.print(analogRead(enA));
+  demoTwo();
+  Serial.print("Demo 2\n");
+  delay(1000);
+  Serial.print(analogRead(enA));
+  */
+  //forward();
   Serial.print("Left Count:");
   Serial.println(leftCount);
-  delay(500);
+  Serial.print("Right Count:");
+  Serial.println(rightCount);
+  delay(1000);
+  
 }
